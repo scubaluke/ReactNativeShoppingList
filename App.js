9502dfb9e74a9f035e469aa322c37b4832c80d5d
Luke Gartland <lukegartland@gmail.com>
2021-06-12 08:09:06 -0500

    added listitem and delete function

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {uuid} from 'uuidv4';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import uuid from 'react-native-uuid';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 const App = () => {
   const [items, setItems] = useState([
-    {id: uuid(), text: 'Milk'},
-    // {id: uuid(), text: 'Eggs'},
-    // {id: uuid(), text: 'Bred'},
-    // {id: uuid(), text: 'Cheese'},
+    {id: uuid.v4(), text: 'Milk'},
+    {id: uuid.v4(), text: 'Eggs'},
+    {id: uuid.v4(), text: 'Bred'},
+    {id: uuid.v4(), text: 'Cheese'},
   ]);
+
+  function deleteItem(id) {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
