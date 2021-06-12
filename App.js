a374d3d94a7ba3e8dad2284dd6ab13a60399d42c
Luke Gartland <lukegartland@gmail.com>
2021-06-11 22:23:54 -0500

    basic styling and components

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>hello world</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
+        style={styles.img}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {color: 'red', fontSize: 30},
+  img: {width: 100, height: 100, borderRadius: 50},
+});
+
+export default App;
